cb83881f0b7c599275e8b931541c6667fe6ad335
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-20 14:22:20 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -7,10 +7,13 @@ int alertFailureCount = 0;
 
 int networkAlertStub(float celcius) {
     printMessage(celcius);
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
     return 200;
 }
 
-bool alertInCelcius(float farenheit) {
+void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
@@ -19,9 +22,7 @@ bool alertInCelcius(float farenheit) {
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
-        return true;
     }
-    return false;
 }
 
 int main() {
