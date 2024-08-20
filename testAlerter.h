87ece405cd2c582e399fc203fccf907d79f511a6
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-20 19:50:40 +0530

    Update testAlerter.h

--- testAlerter.h
+++ testAlerter.h
@@ -1,12 +1,4 @@
 
-int networkAlertMock(float celcius) {
-    printMessage(celcius);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
-}
-
 int networkAlerter(float celcius) {
     printMessage(celcius);
     // Return 200 for ok
