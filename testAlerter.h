49e33c4a5bab38ce9658721e4dbb5c2dd8315c52
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-08-20 19:50:10 +0530

    Update testAlerter.h

--- testAlerter.h
+++ testAlerter.h
@@ -7,3 +7,23 @@ int networkAlertMock(float celcius) {
     return 200;
 }
 
+int networkAlerter(float celcius) {
+    printMessage(celcius);
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
+    return 200;
+}
+
+void alertInCelcius(float farenheit,int (*networkAlert)(float)) {
+    float celcius = (farenheit - 32) * 5 / 9;
+    int returnCode = networkAlert(celcius);
+    if (returnCode != 200) {
+        // non-ok response is not an error! Issues happen in life!
+        // let us keep a count of failures to report
+        // However, this code doesn't count failures!
+        // Add a test below to catch this bug. Alter the stub above, if needed.
+        alertFailureCount += 0;
+    }
+}
+
